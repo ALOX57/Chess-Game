66c7f8f5177a5f434118e7b8f17635f9f05d19dd
Alexander Birtles <aloxemail@gmail.com>
2024-12-01 16:09:22 +1100

    Add all chess pieces to board

--- src/pieces/Pawn.java
+++ src/pieces/Pawn.java
@@ -15,7 +15,7 @@ public class Pawn extends Piece{
         this.isWhite = isWhite;
         this.name = "Pawn";
 
-        this.sprite =  sheet.getSubimage(6 * sheetScale, isWhite ? 0 : sheetScale, sheetScale, sheetScale)
+        this.sprite =  sheet.getSubimage(5 * sheetScale, isWhite ? 0 : sheetScale, sheetScale, sheetScale)
                 .getScaledInstance(board.tileSize, board.tileSize, BufferedImage.SCALE_SMOOTH);
     }
 }
