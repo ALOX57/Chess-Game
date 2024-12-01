5781ffa70383e529a82a8c52e33c87ea05993a7c
Alexander Birtles <aloxemail@gmail.com>
2024-12-01 15:28:55 +1100

    Add a white black checker pattern to board

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         JFrame frame = new JFrame();
+        frame.setLayout(new GridBagLayout());
         frame.setMinimumSize(new Dimension(1000,1000));
         frame.setLocationRelativeTo(null);
 
