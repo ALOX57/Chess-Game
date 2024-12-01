0861d7de4dfe1272c9a76ddae668c38b29fb1fa5
Alexander Birtles <aloxemail@gmail.com>
2024-12-01 15:31:17 +1100

    BUGFIX: Fix black and white checker pattern for loop

--- src/Main/Board.java
+++ src/Main/Board.java
@@ -10,17 +10,17 @@ public class Board extends JPanel {
     int rows = 8;
 
     public Board() {
-        this.setPreferredSize(new Dimension(cols * tileSize, rows *tileSize));
+        this.setPreferredSize(new Dimension(cols * tileSize, rows * tileSize));
     }
 
     public void paintComponent(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
 
         for (int r = 0; r < rows; r++)
-        for (int c = 0; r < rows; c++) {
-            g2d.setColor((c + r) % 2 == 0 ? Color.white : Color.black);
-            g2d.fillRect(c * tileSize, r * tileSize, tileSize, tileSize);
-        }
+            for (int c = 0; c < rows; c++) {
+                g2d.setColor((c + r) % 2 == 0 ? Color.white : Color.black);
+                g2d.fillRect(c * tileSize, r * tileSize, tileSize, tileSize);
+            }
 
     }
 }
