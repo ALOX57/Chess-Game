d95e7de4f192b15ba1cb6bc93599841f74d05de9
Alexander Birtles <aloxemail@gmail.com>
2025-01-30 17:51:21 +1100

    Highlights validMoves when selected a piece

--- src/main/Board.java
+++ src/main/Board.java
@@ -109,13 +109,25 @@ public class Board extends JPanel {
     public void paintComponent(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
 
+        // paint board
         for (int r = 0; r < rows; r++)
             for (int c = 0; c < rows; c++) {
                 g2d.setColor((c + r) % 2 == 0 ? new Color(205, 189, 175) : new Color(114, 94, 76));
                 g2d.fillRect(c * tileSize, r * tileSize, tileSize, tileSize);
             }
 
+        // paint highlights
+        if (selectedPiece != null)
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < rows; c++) {
+                if (isValidMove(new Move(this, selectedPiece, c, r))) {
+                    g2d.setColor(new Color(37, 214, 29, 190));
+                    g2d.fillRect(c * tileSize, r * tileSize, tileSize, tileSize);
+                }
+            }
+        }
 
+        // paint pieces
         for (Piece piece : pieceList) {
             piece.paint(g2d);
         }
