a5696caa786da37a99ae594f72dcfebb5acba3e0
Alexander Birtles <aloxemail@gmail.com>
2025-01-31 14:50:18 +1100

    Add Rook collision logic

--- src/pieces/Rook.java
+++ src/pieces/Rook.java
@@ -23,4 +23,34 @@ public class Rook extends Piece{
         return this.col == col || this.row == row;
     }
 
+    public boolean moveCollidesWithPiece(int col, int row) {
+
+        // left
+        if (this.col > col)
+            for (int c = this.col - 1; c > col; c--)
+                if (board.getPiece(c, this.row) != null)
+                    return true;
+
+        // right
+        if (this.col < col)
+            for (int c = this.col + 1; c < col; c++)
+                if (board.getPiece(c, this.row) != null)
+                    return true;
+
+        // up
+        if (this.row > row)
+            for (int r = this.row - 1; r > row; r--)
+                if (board.getPiece(this.col, r) != null)
+                    return true;
+
+        // down
+        if (this.row < row)
+            for (int r = this.row + 1; r < row; r++)
+                if (board.getPiece(this.col, r) != null)
+                    return true;
+
+
+        return false;
+    }
+
 }
